71529a96f7c2fa2b275f26de3932b71bda773ed6
Your Name <you@example.com>
2018-01-24 22:46:57 -0500

    Backing up

--- research.py
+++ research.py
@@ -4,31 +4,31 @@ import json
 import pdb
 
 class research(object):
-    
+
     def __init__(self,config):
         if 'service' in config:
             self.config = global_config[config['service']].copy()
         else:
             self.config = global_config['default'].copy()
-        
+
         if config is not None:
             self.config.update(config)
-        
+
         self.api = research.buildApiConnector(self,None)
-    
+
     @staticmethod
     def buildApiConnector(self,config):
         if config is not None:
             self.updateConfig(config)
-        
+
     	if self.config['service']:
     		if self.config['service']=='intrinio':
     			connector = intrinioApi(self)
     	else:
     		connector = apiConnector(self.config)
-        
+
         return connector
-    
+
     @staticmethod
     def updateConfig(self,config):
         self.config.update(config)
@@ -38,9 +38,9 @@ class research(object):
 
     	if self.config['json']:
     		content = json.loads(content)
-		
+
     	return content
-    
+
     # def exchangeInfo(self,config):
     #     content = research.buildApiConnector(self,config).initExchangeData(self)
     #
@@ -48,7 +48,7 @@ class research(object):
     #         content = json.loads(content)
     #
     #     return content
-        
+
 
     # def backfillHistoricals(config):
     #
--- utils/api.py
+++ utils/api.py
@@ -8,78 +8,70 @@ import datetime
 class apiConnector(object):
     def __init__(self,parent):
         if isinstance(parent,object):
-            config = parent.config
-        elif parent
-        if 'username' in parent:
-            self.username = config['username']
-        if 'password' in config:
-            self.password = config['password']
-        if 'credentials' in config:
-            self.credentials = config['credentials']
-        if 'parameters' in config:
-            self.parameters = config['parameters']
-        elif hasattr(self,'config'):
-            if hasattr(self,'config'):
-                pass
-        
+            self.config = parent.config
+        elif type(parent) is dict:
+            if 'username' in parent:
+                self.username = parent['username']
+            if 'password' in parent:
+                self.password = parent['password']
+            if 'credentials' in parent:
+                self.credentials = parent['credentials']
+            if 'parameters' in parent:
+                self.parameters = parent['parameters']
+            elif hasattr(self,'config'):
+                if hasattr(self,'config'):
+                    pass
+
         self.now = datetime.datetime.now()
         self.today_str = self.now.strftime('%Y-%m-%d')
-        
+
     def callApi(self,config):
-        
-        pdb.set_trace()
-        if config is not None:
-        	if 'headers' in config:
-        		request_headers = config['headers']
-        	if 'url' in config:
-        		request_url = config['url']
-        	if 'data' in config:
-        		data = config['data']
+
+        if config is None and hasattr(self,'config'):
+            config = self.config
+
+    	if 'headers' in config:
+    		request_headers = config['headers']
+
+    	if 'data' in config:
+    		data = config['data']
         else:
-            if hasattr(self,'data'):
-                data = self.data
-            else:
-                data = None
-                
-            if hasattr(self,'headers'):
-                request_headers = self.headers
-            else:
-                request_headers = None
-                
-            if hasattr(self,'request_url'):
-                request_url = self.request_url
-            elif hasattr(self,'url'):
-                request_url = self.url
-            else:
-                return 'A URL endpoint is required to make an HTTP call'
-        
+            data = None
+
+        if 'request_url' in config:
+            request_url = config['request_url']
+        elif 'url' in config:
+            request_url = config['url']
+        else:
+            return 'A URL endpoint is required to make an HTTP call'
+
         if request_headers:
         	request = urllib2.Request(request_url, headers=request_headers)
         else:
             request = urllib2.Request(request_url)
-            
+
         contents = urllib2.urlopen(request,data).read()
 
     	return contents
-	
+
 class intrinioApi(apiConnector):
     def __init__(self,parent):
         super(intrinioApi, self).__init__(parent)
-            
+
 
     def initScreen(self,config):
         self.url = 'https://api.intrinio.com/securities/search?'
-        
         if config is not None:
             if 'parameters' in config:
-            	self.request_url = self.url + 'conditions=' + config['parameters']
-        elif self.parameters:
-            self.request_url = self.url + self.parameters
+            	self.config['request_url'] = self.config['url'] + 'conditions=' + self.config['parameters']
+        elif self.config:
+            if 'parameters' in self.config:
+                self.config['request_url'] = self.url + 'conditions=' + self.config['parameters']
 
-        data = apiConnector.callApi(self,config)
+        data = apiConnector.callApi(self, config=None)
 
         return data
-        
+
     # def initExchangeData(self,config):
 #         pdb.set_trace()
 #         self.url = 'https://api.intrinio.com/prices/exchange?'
